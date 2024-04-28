7adc2ebd1bdb9b919b79b8758351bbad4e2c461f
mpnodar <57813861+mpnodar@users.noreply.github.com>
2024-04-27 22:11:21 -0400

    Update dataStructuresProject.cpp

--- dataStructuresProject.cpp
+++ dataStructuresProject.cpp
@@ -9,19 +9,78 @@
 #include "Location.h"
 #include "Merchant.h"
 #include "GameMap.h"
-#include "gameMenu.h"
-#include "characters.h"
+#include "gameMenu.cpp"
+#include "characters.cpp"
+
+
+static void characterSelect(player& you_) {
+
+	int input;
+	string name;
+
+	cout << "Please select your character: \n\n----------------------\n\n1. Warrior\nHealth: 70\nMagicka: 30\nStamina: 60\nAttack Damage: 15" << endl;
+	cout << "\n----------------------\n\n2. Mage\nHealth: 40\nMagicka: 90\nStamina: 50\nAttack Damage: 5" << endl;
+	cout << "\n----------------------\n\n3. Scout\nHealth: 55\nMagicka: 50\nStamina: 70\nAttack Damage: 9\n\n----------------------\n" << endl;
+
+	cin >> input;
+
+	switch (input) {
+	case 1:
+		you_.setMaxHealth(70);
+		you_.setHealth(70);
+		you_.setMaxMagicka(30);
+		you_.setMaxStamina(60);
+		you_.setStamina(60);
+		you_.setAttack(15);
+
+		cout << "Please enter a name for your character: ";
+		cin >> name;
+		you_.setName(name);
+		break;
+	case 2:
+		you_.setMaxHealth(40);
+		you_.setHealth(40);
+		you_.setMaxMagicka(90);
+		you_.setMaxStamina(50);
+		you_.setStamina(50);
+		you_.setAttack(5);
+
+		cout << "Please enter a name for your character: ";
+		cin >> name;
+		you_.setName(name);
+		break;
+
+
+	case 3:
+		you_.setMaxHealth(55);
+		you_.setHealth(55);
+		you_.setMaxMagicka(30);
+		you_.setMaxStamina(50);
+		you_.setStamina(50);
+		you_.setAttack(9);
+
+		cout << "Please enter a name for your character: ";
+		cin >> name;
+		you_.setName(name);
+		break;
+
+
+	}
+
+}
 
 
 int main() {
 
 	player p;
 
+	characterSelect(p);
+
 	gameMenu menu;
 	menu.menu(&p);
 
-	GameMap map;
-	map.play();
+	/*GameMap map;
+	map.play(); */
 
 	/*RoadsideBeggar jim("Jim");
 	jim.printDialogue(0);*/
@@ -37,7 +96,7 @@ int main() {
 	//player you(1, name);
 
 	//menu(you);
-	
+
 
 
 	return 0;
